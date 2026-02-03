d4aef535fd569033530a406d52dbcdceb15d2692
HaBaisie <lawalbaisie1@gmail.com>
2026-02-03 18:15:16 +0100

    second

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+gunicorn ppn.wsgi:application --bind 0.0.0.0:$PORT --log-file -
